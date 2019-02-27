4747694da49c8371ae27c40f4510e8cd2c76e115
Donovan <Donovan.Taitt@yahoo.com>
2019-02-26 21:37:48 -0500

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
